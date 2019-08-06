09e3f89841ec7c4a4e7e5acafcf170947b76e2d5
Suraj Singh Rana <surajsinghrana7417@gmail.com>
2019-08-06 11:36:34 +0530

    Client Class updated

--- Client.java
+++ Client.java
@@ -1,17 +1,60 @@
+import java.net.Socket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.io.IOException;
+
 public class Client{
 	
-	String Name;
 	Socket skt;
+	String name;
 	
-	
-	Client(String Name,Socket skt){
+	public Client(Socket skt,String name){
 		
-		this.Name = Name;
+		this.name = name; 
 		this.skt = skt;
 		
-		ClientReader ClReader = new ClientReader(skt.getInputStream());
-		ClientWriter ClWriter = new ClientWriter(skt.getOutputSteam());
-	
+		try{
+
+			ClientReader ClReader = new ClientReader(new DataInputStream(skt.getInputStream()));
+			ClientWriter ClWriter = new ClientWriter(new DataOutputStream(skt.getOutputStream()));
+		
+		}
+
+		catch(Exception ex){
+
+			System.out.println("client initialization failed :: "+ex.getMessage());
+		}
+
+	}
+
+	public static void main(String args[]){
+
+		try{
+
+			InetAddress host = InetAddress.getByName(args[0]);
+
+			int portNo = Integer.parseInt(args[1]);
+
+			String name = args[2];
+
+			System.out.println("Connecting to server");
+
+			Socket skt = new Socket(host,portNo);
+
+			Client cl = new Client(skt,name);
+		}
+
+		catch(ArrayIndexOutOfBoundsException ex){
+
+			System.out.println("Client < host > < port > < client-name >");
+		}
+
+		catch(Exception ex){
+
+			System.out.println(ex.getMessage());
+
+		}
 	}
 
 
